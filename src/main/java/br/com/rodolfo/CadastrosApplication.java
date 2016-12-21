d1b6201c96ffd71f68bf401317c7980c2e358607
Rodolfo_Herman <ciencia.rodolfo@gmail.com>
2016-12-21 20:45:39 -0200

    DataSourcePost

--- src/main/java/br/com/rodolfo/CadastrosApplication.java
+++ src/main/java/br/com/rodolfo/CadastrosApplication.java
@@ -3,9 +3,7 @@ package br.com.rodolfo;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -13,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CadastrosApplication {
 
 	public static void main(String[] args) {
