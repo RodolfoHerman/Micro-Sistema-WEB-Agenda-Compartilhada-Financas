127a4208aea9ea026c85660a45874a8b9ff0a26f
Rodolfo_Herman <ciencia.rodolfo@gmail.com>
2016-12-22 10:49:17 -0200

    JavaxValid

--- src/main/java/br/com/rodolfo/controller/TituloController.java
+++ src/main/java/br/com/rodolfo/controller/TituloController.java
@@ -3,10 +3,11 @@ package br.com.rodolfo.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,13 +44,7 @@ public class TituloController {
 	}
 	
 	@RequestMapping (method = RequestMethod.POST)
-	public String salvar (Titulo titulo, Errors errors, RedirectAttributes attributes) {
-		
-		
-		if ((titulo.getStatus() != null) && (titulo.getStatus().equals(StatusTitulo.BRANCO.getDescricao()))) {
-			
-			errors.rejectValue("status", null, "Status é obrigatório");			
-		}
+	public String salvar (@Valid Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		
 		if (titulo.isRecebido()) {
 			
