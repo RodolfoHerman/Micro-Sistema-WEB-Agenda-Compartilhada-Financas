7f6796e5ec36848d5616923183795463c6413fa3
Rodolfo_Herman <ciencia.rodolfo@gmail.com>
2016-12-22 10:55:01 -0200

    BeanV

--- src/main/java/br/com/rodolfo/controller/TituloController.java
+++ src/main/java/br/com/rodolfo/controller/TituloController.java
@@ -3,11 +3,10 @@ package br.com.rodolfo.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +43,7 @@ public class TituloController {
 	}
 	
 	@RequestMapping (method = RequestMethod.POST)
-	public String salvar (@Valid Titulo titulo, Errors errors, RedirectAttributes attributes) {
+	public String salvar (@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		
 		if (titulo.isRecebido()) {
 			
