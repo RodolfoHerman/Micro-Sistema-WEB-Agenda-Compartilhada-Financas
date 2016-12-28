ee87dffad6c528ff207f6a37852db0e57810b680
Rodolfo_Herman <ciencia.rodolfo@gmail.com>
2016-12-27 22:33:40 -0200

    cliente_0.1

--- src/main/java/br/com/rodolfo/CadastrosApplication.java
+++ src/main/java/br/com/rodolfo/CadastrosApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
