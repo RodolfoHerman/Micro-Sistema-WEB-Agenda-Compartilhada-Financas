7f1a0f45ea5be256bd9bf7d2fe767082c8591ee3
Rodolfo_Herman <ciencia.rodolfo@gmail.com>
2017-09-02 12:20:17 -0300

    atualização

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,88 +1,79 @@
-$('#confirmacaoExclusaoModal').on('show.bs.modal', function (event){
-	
-	var button = $(event.relatedTarget);
-	var codigo = button.data('codigo');
-	var descricao = button.data('descricao');
-	
-	var modal = $(this);
+function excluir(dom) {
+
+	let btnTitulos = 'btnExcluirPesquisaTitulos';
+	var name = dom.getAttribute("name");
+		
+	var modal = $('#confirmacaoExclusaoModal');
 	var form = modal.find('form');
-	var actionText = form.data('url-base');
-	
-	console.log(actionText);
-	
-	if (!actionText.endsWith('/')) {
+	var src = form.attr('data-url-base');
 		
-		actionText = actionText + '/';
-
+	if (name == btnTitulos) {
+		
+		var dst = src.substring(0, src.lastIndexOf("/")+1) + "titulos";
+		form.attr('data-url-base', dst);		
+	} else {
+			
+		var dst = src.substring(0, src.lastIndexOf("/")+1) + "agenda";
+		form.attr('data-url-base', dst);
 	}
-	
-	form.attr ('action', actionText + codigo);
-	
-	modal.find('.modal-body span').html('Tem certeza que quer excluir <strong>' + descricao + '</strong> ?');
-	
-});
+}
 
 
+$(document).ready(function () {	
 
-$(function(){
-	
-	$('[name="button"]').tooltip();
-	$('[rel="tooltip"]').tooltip();
-	$('.js-formatar-valor').maskMoney({decimal:",", thousands:".", allowZero:"false"});
-	
-});
-
-$(function(){
-	
-	$('.js-verificar-selecao').on('change', function (event){
+	$('#confirmacaoExclusaoModal').on('show.bs.modal', function (event){
 		
-		var selecao = event.currentTarget.value;
+		var button = $(event.relatedTarget);
+		var codigo = button.data('codigo');
+		var descricao = button.data('descricao');
 		
-		if (selecao == 'RECEBIDO') {
-			document.getElementById('div_dataPg').style.display = "block";
-			document.getElementById('div_valorPg').style.display = "block";
-		    
-		} else {
-			document.getElementById('div_dataPg').style.display = "none";
-			document.getElementById('div_valorPg').style.display = "none";
+		var modal = $(this);
+		var form = modal.find('form');
+		var actionText = form.data('url-base');
+		
+		console.log(actionText);
+		
+		if (!actionText.endsWith('/')) {
+			
+			actionText = actionText + '/';
+
 		}
 		
+		form.attr ('action', actionText + codigo);
+		
+		modal.find('.modal-body span').html('Tem certeza que quer excluir <strong>' + descricao + '</strong> ?');
 		
 	});
-	
-	
-	$('.js-botao-excluir').on('click', function(){
-		
-		let btnTitulos = 'btnExcluirPesquisaTitulos';
-		var id = jQuery(this).attr("id");
-		
-		var modal = $('#confirmacaoExclusaoModal');
-		var form = modal.find('form');
-		var src = form.attr('data-url-base');
-		
-		if (id == btnTitulos) {
-			
-			var dst = src.substring(0, src.lastIndexOf("/")+1) + "titulos";
 
-			form.attr('data-url-base', dst);
-			
+	//Adiciona um tooltipo ao elemento, a selecao acontece pelo atributo name e pela atributo rel
+	$('[name="button"]').tooltip();
+	$('[rel="tooltip"]').tooltip();
+	//Utiza-se o seletor de classe para atribuir a mascara aos elementos
+	$('.js-formatar-valor').maskMoney({decimal:",", thousands:".", allowZero:"false"});
+
+
+	//Quando ha mudanca no componete select, verifica-se seu estado e realizada as alteracoes.
+	$('#verificar-selecao').on('change', function() {
+		
+		var selecao = $(this).val();
+		
+		if (selecao == 'RECEBIDO') {
+			$('#div_dataPg').show();
+			$('#div_valorPg').show();
+		    
 		} else {
-			
-			
-			var dst = src.substring(0, src.lastIndexOf("/")+1) + "agenda";
-
-			form.attr('data-url-base', dst);
+			$('#div_dataPg').val("").hide();
+			$('#div_valorPg').val("").hide();
 		}
-		
 		
 	});	
 	
-	$('.js-limpar-campos').on('click', function(event){
+	$('#limpar-campos').on('click', function(event){
 		
 		event.preventDefault();
 		
 		$('#formPesquisa').find("input[type=text]").val(null);
 		$('#formPesquisa').trigger('submit');
-	});
-	
-});
+	});	
+
+}
